c137b73ab441c4634ed440a6c0689754a1b722c1
Maciej Janowski <maciej.janowski1@icloud.com>
2024-06-10 19:43:22 -0300

    update modal

--- src/lib/components/modals/CreateHabitModal.svelte
+++ src/lib/components/modals/CreateHabitModal.svelte
@@ -9,7 +9,6 @@
     <div class="modal-box">
         <h3 class="font-bold text-lg">Create a Habit</h3>
         <form method="POST" action="?/createHabit">
-            <input value={statusAdd} type="hidden" name="status">
             <div>
                 <label class="w-full max-w-xs">
                     <div class="label">
@@ -18,6 +17,16 @@
                     <input required type="text" name="name" class="input input-bordered w-full max-w-lg">
                 </label>
             </div>
+            <label class="form-control w-full max-w-lg">
+                <div class="label">
+                    <span class="label-text">Habit type</span>
+                </div>
+                <select name="status" class="select select-bordered w-full">
+                    <option selected={statusAdd === "core"} value="core">Daily Routine</option>
+                    <option selected={statusAdd === "wishlist"} value="wishlist">Wishlist</option>
+                    <option selected={statusAdd === "wip"} value="wip">Work on</option>
+                </select>
+            </label>
             <div class="modal-action">
                 <button class="btn btn-primary" type="submit">Create</button>
                 <button type="button" on:click={() => createModal.close()} class="btn">Cancel</button>
